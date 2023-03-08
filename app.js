d70352afc79047e5c83303cd794846ddfbf7f439
EazyXxX <104682962+EazyXxX@users.noreply.github.com>
2023-03-08 17:38:39 +0300

    Исправлены баги

--- app.js
+++ app.js
@@ -10,33 +10,26 @@ const cards = require('./routes/cards');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  req.user = {
-    _id: '6400cf0a2c6ea103619eac89',
-  };
-
+  req.user = { _id: '6400cf0a2c6ea103619eac89' };
   next();
 });
 
 app.use('/users', users);
 app.use('/cards', cards);
 
-app.use('*', (req, res) => res.status(CodeError.BAD_REQUEST).send({ message: 'Страница не существует.' }));
+app.use('*', (req, res) => res.status(CodeError.BAD_REQUEST).send({ message: 'Страница не существует' }));
+
+mongoose.set('runValidators', true);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 }, () => {
   console.log('Подключено к MongoDB');
-});
 
-mongoose.set('runValidators', true);
-
-app.use(express.json());
-
-app.listen(PORT, (error) => {
-  error ? console.log(error) : console.log(`App listening on port ${PORT}`);
+  app.listen(PORT, (error) => (error ? console.error(error) : console.log(`App listening on port ${PORT}`)));
 });
