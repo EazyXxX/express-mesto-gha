f703ac2efc3dc51dc1f9bef6a706c97c992295a6
EazyXxX <104682962+EazyXxX@users.noreply.github.com>
2023-03-23 15:37:50 +0300

    Исправлены баги

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('express').Router();
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cors = require('cors');
-const { CodeError } = require('./statusCode');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
+const { login, register } = require('./controllers/users');
 const authMiddleware = require('./middlewares/auth');
 
 mongoose.set('strictQuery', false);
-
 const app = express();
-
 const PORT = 3000;
 
 app.use(helmet());
@@ -23,15 +19,11 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
-
-app.use(authMiddleware);
+app.post('/login', login);
+app.post('/register', register);
 
 app.use('/users', users);
 app.use('/cards', cards);
-
-app.use('*', (req, res) => res.status(CodeError.NOT_FOUND).send({ message: 'Страница не существует' }));
 
 app.use(errors());
 
@@ -41,11 +33,9 @@ app.use((err, req, res, next) => {
 });
 
 mongoose.set('runValidators', true);
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 }, () => {
   console.log('Подключено к MongoDB');
-
   app.listen(PORT, (error) => (error ? console.error(error) : console.log(`App listening on port ${PORT}`)));
 });
