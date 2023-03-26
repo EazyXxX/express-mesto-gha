d089388aa9f93786bff8f2a9a4f1ac6339d6673d
EazyXxX <104682962+EazyXxX@users.noreply.github.com>
2023-03-26 17:49:57 +0300

    Коммит для прохождения автотестов

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,23 @@
+/* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
 
-const authMiddleware = (err, req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    res.status(401).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
-  // попытаемся верифицировать токен
-  jwt.verify(token, JWT_SECRET)
-    .catch((e) => {
-      next(e);
-    });
+  try {
+    // попытаемся верифицировать токен
+    payload = await jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    next(err);
+  }
   req.user = payload;
   next();
 };
