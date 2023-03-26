b2b38defe8ee5a5b303db0c7c560bdb838e6132f
EazyXxX <104682962+EazyXxX@users.noreply.github.com>
2023-03-26 20:48:45 +0300

    Коммит для прохождения автотестов

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     password: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
   }),
 }), signup);
 
