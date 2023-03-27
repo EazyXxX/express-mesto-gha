d9afbe3cc3aa2f8da790c193d4b8a925202dab32
EazyXxX <104682962+EazyXxX@users.noreply.github.com>
2023-03-27 18:28:17 +0300

    Коммит для прохождения автотестов

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,10 @@ const { JWT_SECRET } = require('../config');
 const authMiddleware = async (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    res.status(401).send({ message: 'Необходима авторизация' });
   }
-
+  console.log(authorization);
   const token = authorization.replace('Bearer ', '');
   let payload;
 
